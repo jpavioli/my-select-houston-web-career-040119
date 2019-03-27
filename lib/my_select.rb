eaf511c38adecc29b5aab58bac2976db7fab7116
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-27 17:53:50 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,4 +1,19 @@
 def my_select(collection)
  # your code here!
- collection.select
+  counter = 0
+  new_collection = []
+  if collection == [] 
+    new_collection = []
+  else 
+    collection_count = collection.size  
+    while counter < collection_count
+      new_value = yield collection[counter]
+      new_collection << new_value
+      counter += 1
+    end
+  end
+  new_collection
 end
+
+my_select("It Me") {|i| "#{i.upcase}"}
+
